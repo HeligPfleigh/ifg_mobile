f29f565544fbd44fa9e8110fd4e922b2341ef5df
Linh Le <linh.le@mttjsc.com>
2019-08-26 19:44:04 +0700

    Coding Style Configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  rules: {
+    'func-names': 'off',
+    'import/no-cycle': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-prototype-builtins': 'off',
+    'no-underscore-dangle': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/destructuring-assignment': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react/no-array-index-key': 'off',
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
 };
