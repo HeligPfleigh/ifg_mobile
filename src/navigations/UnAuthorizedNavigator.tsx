db28984fbf883c42ba136934cabf1de36507185c
“Hieu <hieu.le@mttjsc.com>
2019-09-03 17:12:28 +0700

    feat: Add navigation for sign up screen, forgot password screen and license screen

--- src/navigations/UnAuthorizedNavigator.tsx
+++ src/navigations/UnAuthorizedNavigator.tsx
@@ -2,12 +2,18 @@ import { createStackNavigator } from 'react-navigation';
 
 import NavigatorMap from './NavigatorMap';
 import SignInScreen from '../screens/SignIn';
+import SignUpScreen from '../screens/SignUp';
 import WelcomeScreen from '../screens/Welcome';
+import LicenseScreen from '../screens/License';
+import ForgotPasswordScreen from '../screens/ForgotPassword';
 
 export const AuthStack = createStackNavigator(
   {
     [NavigatorMap.Welcome]: WelcomeScreen,
+    [NavigatorMap.License]: LicenseScreen,
     [NavigatorMap.SignIn]: SignInScreen,
+    [NavigatorMap.SignUp]: SignUpScreen,
+    [NavigatorMap.ForgotPassword]: ForgotPasswordScreen,
   },
   {
     defaultNavigationOptions: {
