c0664b5b44ec04cb421773814dc8675e90b59b59
“Hieu <hieu.le@mttjsc.com>
2019-09-05 13:02:01 +0700

    chore: Remove unused Header component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,7 +4,6 @@ import * as FormFields from './FormFields';
 import EvaluationItem, { RoundIconButton } from './EvaluationItem';
 import ScoreText from './ScoreText';
 import ModalContainer from './modal/ModalContainer';
-import Header from './Header';
 import Checkbox from './Checkbox';
 
-export { Block, Button, EvaluationItem, ScoreText, ModalContainer, RoundIconButton, Header, FormFields, Checkbox };
+export { Block, Button, EvaluationItem, ScoreText, ModalContainer, RoundIconButton, FormFields, Checkbox };
