a8ddb583a069cea6d8a022ad91a2043c793847af
goldenhands92 <tuan.tran@mttjsc.com>
2019-09-11 09:55:55 +0700

    chore: add react native firebase and config that lib

--- src/screens/AuthLoading.tsx
+++ src/screens/AuthLoading.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
+import firebase from 'react-native-firebase';
 
 import { useSelector } from 'react-redux';
 import { AppState } from '../store/types';
@@ -18,6 +19,47 @@ const AuthLoadingScreen: React.FC<AuthLoadingProps> = (props: AuthLoadingProps) 
   const authToken = useSelector((state: AppState) => state.auth.token);
   authorizeApi(authToken);
   navigation.navigate(authToken ? NavigatorMap.App : NavigatorMap.Auth);
+  const getToken = async () => {
+    const fcmToken = await firebase.messaging().getToken();
+    console.log(fcmToken);
+  };
+
+  const requestPermission = async () => {
+    try {
+      await firebase.messaging().requestPermission();
+    } catch (error) {
+      // TODO
+    }
+  };
+
+  const checkPermission = async () => {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      getToken();
+    } else {
+      requestPermission();
+    }
+  };
+
+  const createNotificationListeners = async () => {
+    firebase.notifications().onNotification(notification => {
+      notification.android.setChannelId('insider').setSound('default');
+      firebase.notifications().displayNotification(notification);
+    });
+  };
+
+  useEffect(() => {
+    const channel = new firebase.notifications.Android.Channel(
+      'insider',
+      'insider channel',
+      firebase.notifications.Android.Importance.Max,
+    );
+    firebase.notifications().android.createChannel(channel);
+    checkPermission();
+    createNotificationListeners();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <Block middle>
       <ActivityIndicator size="large" color={theme.colors.blue} />
