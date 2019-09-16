e320fa44c51738a894b27720c6b82fd0defc10c8
goldenhands92 <tuan.tran@mttjsc.com>
2019-09-16 17:19:20 +0700

    chore: show meaningful error in status 409, redirect user to Home right after signup

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, TouchableOpacity, View, KeyboardAvoidingView, Keyboard } from 'react-native';
 import { connect } from 'react-redux';
-import { Field, reduxForm, formValueSelector } from 'redux-form';
-import { withNavigation } from 'react-navigation';
+import { Field, reduxForm, formValueSelector, InjectedFormProps } from 'redux-form';
 import { ScrollView } from 'react-native-gesture-handler';
 import { noop } from 'lodash';
+import get from 'lodash/get';
+import { NavigationScreenProps } from 'react-navigation';
+import NavigatorMap from '../../navigations/NavigatorMap';
 import I18n from '../../core/i18n';
-import { showModal } from '../../store/actions';
+import { showModal, login } from '../../store/actions';
 import { TextField, FormValidator as validator } from '../../components/FormFields';
 import { Block, Button, Checkbox, Toast, Loader } from '../../components';
 import { theme, Enum } from '../../constants';
@@ -14,18 +16,31 @@ import api from '../../core/api';
 import { styles } from './styles';
 import { ReduxFormName } from '../../constants/enum';
 
-interface SignUpProps {
+interface SignUpProps extends InjectedFormProps, NavigationScreenProps {
   dispatch: any;
   password: any;
+  authToken: any;
 }
 
-class SignUp extends React.Component<SignUpProps> {
-  constructor(props) {
+interface SignUpStates {
+  isChecked: boolean;
+  loading: boolean;
+}
+
+class SignUp extends React.Component<SignUpProps, SignUpStates> {
+  constructor(props: SignUpProps) {
     super(props);
     this.state = {
       isChecked: false,
       loading: false,
     };
+  }
+
+  componentDidUpdate() {
+    const { authToken, navigation } = this.props;
+    if (authToken) {
+      navigation.navigate(NavigatorMap.Home);
+    }
   }
 
   _showLicenseModal = () => this.props.dispatch(showModal({ onModalPress: noop, modalType: Enum.ModalType.LICENSE }));
@@ -34,16 +49,25 @@ class SignUp extends React.Component<SignUpProps> {
     return validator.comparePassword(this.props.password, value);
   };
 
-  _onSignup = async value => {
+  _onSignup = async (value: any) => {
     Keyboard.dismiss();
     this.setState({ loading: true });
+    const { dispatch } = this.props;
     try {
       const { username, email, password } = value;
       const bundleData = { username, email, password };
       await api.signup(bundleData);
       Toast.success(I18n.t('signup.success'));
+      dispatch(login(email, password));
     } catch (err) {
-      Toast.error(err.message);
+      let message;
+      const statusCode = get(err, 'response.data.error.statusCode', 400);
+      if (statusCode === 409) {
+        message = I18n.t('signup.email_not_available');
+      } else {
+        message = get(err, 'response.data.error.message', err.message);
+      }
+      Toast.error(message);
     } finally {
       this.setState({ loading: false });
     }
@@ -57,7 +81,7 @@ class SignUp extends React.Component<SignUpProps> {
       <React.Fragment>
         <Loader loading={loading} />
         <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
-          <ScrollView style={styles.container}>
+          <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
             <Block flex={false} style={{ aspectRatio: 2.8 }} middle>
               <Text style={styles.title}>{I18n.t('signup.title')}</Text>
             </Block>
@@ -94,7 +118,7 @@ class SignUp extends React.Component<SignUpProps> {
               component={TextField}
               autoCorrect={false}
               characterRestriction={120}
-              validate={[required, minLength8, maxLength120, password, this._compareValue]}
+              validate={[required, this._compareValue]}
               tintColor={theme.colors.green}
             />
             <View style={styles.confirm}>
@@ -120,6 +144,7 @@ class SignUp extends React.Component<SignUpProps> {
 
 const mapStateToProps = (state: any) => ({
   password: formValueSelector(ReduxFormName.SIGN_UP)(state, 'password'),
+  authToken: state.auth.token,
 });
 
-export default connect(mapStateToProps)(reduxForm({ form: ReduxFormName.SIGN_UP })(withNavigation(SignUp)));
+export default connect(mapStateToProps)(reduxForm({ form: ReduxFormName.SIGN_UP })(SignUp as any));
