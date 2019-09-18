136d8b5c20e73ea3c2bdc3bc941d99e7883bbc09
Linh Le <linh.le@mttjsc.com>
2019-09-18 15:31:14 +0700

    refactor: remove notifications panel in parameters screen

--- src/screens/Parameters/index.tsx
+++ src/screens/Parameters/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { ScrollView } from 'react-navigation';
-
-import I18n from '../../core/i18n';
-import Notifications from './components/Notifications';
 import LanguageSetting from './components/LanguageSetting';
 import { styles } from './styles';
 
 const Parameters = () => (
   <ScrollView scrollEnabled={false} style={styles.container}>
-    <Notifications />
-    <LanguageSetting language={I18n.locale} />
+    <LanguageSetting />
   </ScrollView>
 );
 
